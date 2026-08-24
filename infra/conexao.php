f69927889d771cf4df5cd6fb6aea7d6b70db31ca
gabrielribeiro-code <gabriel_ribeiro16@estudante.sesisenai.org.br>
2026-08-24 08:12:18 -0300

    add - Adicionado todos os arquivos php e suas respectivas pastas e organização.

---
+++ infra/conexao.php
@@ -0,0 +1,17 @@
+<?php
+
+$host = "localhost: 6608";
+$usuario = "root";
+$senha = "";
+$banco = "restauranteDB";
+
+$conexao = new mysqli($host, $usuario, $senha, $banco);
+
+if ($conexao -> connect_error) {
+    die("Erro na conexão como banco: " . $conexao -> connect_error);
+
+};
+
+$conexao -> set_charset("utf8mb4");
+
+?>
